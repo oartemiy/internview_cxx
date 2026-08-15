f72d46d3f8344b5222fabf7f9164e5013c7bb689
oartemiy <artemiy.obraztsov@yandex.ru>
2026-08-16 00:19:27 +0300

    Small cmake changes

--- cmake/DownloadJwt-cpp.cmake
+++ cmake/DownloadJwt-cpp.cmake
@@ -1,11 +1,15 @@
 include_guard(GLOBAL)
 include(get_cpm)
 
-cpmaddpackage(
-    NAME 
-    jwt-cpp
-    GIT_REPOSITORY
-    https://github.com/Thalhammer/jwt-cpp.git
-    VERSION
-    0.7.2
-)
+find_package(jwt-cpp QUIET)
+
+if(NOT jwt-cpp_FOUND)
+    cpmaddpackage(
+        NAME
+        jwt-cpp
+        GIT_REPOSITORY
+        https://github.com/Thalhammer/jwt-cpp.git
+        VERSION
+        0.7.2
+    )
+endif()
--- cmake/DownloadSodium.cmake
+++ cmake/DownloadSodium.cmake
@@ -1,15 +1,19 @@
 include_guard(GLOBAL)
 include(get_cpm)
 
-cpmaddpackage(
-    NAME 
-    Sodium
-    GIT_REPOSITORY 
-    https://github.com/robinlinden/libsodium-cmake.git
-    GIT_TAG 
-    cfebfd3da486d5a86c644c8b47067e5411c7599c
-    # OPTIONS 
-    # "SODIUM_DISABLE_TESTS ON"
-)
+find_package(sodium QUIET)
 
-set(SODIUM_DISABLE_TESTS ON)
+if(NOT sodium_FOUND)
+    cpmaddpackage(
+        NAME
+        sodium
+        GIT_REPOSITORY
+        https://github.com/robinlinden/libsodium-cmake.git
+        GIT_TAG
+        cfebfd3da486d5a86c644c8b47067e5411c7599c
+        # OPTIONS 
+        # "SODIUM_DISABLE_TESTS ON"
+    )
+
+    set(SODIUM_DISABLE_TESTS ON)
+endif()
