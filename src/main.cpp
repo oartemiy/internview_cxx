954eb328e48f8d09e854a13391217bc22c7f8a33
oartemiy <artemiy.obraztsov@yandex.ru>
2026-08-26 23:32:44 +0300

    Minor update

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,7 @@
 #include "handlers/vacancy/handler_vacancy_post.hpp"
 #include "userver/storages/secdist/provider_component.hpp"
 
-// TODO: add pool for slave HOSTTYPE
+// ?: Add pool for slave HOSTTYPE
 
 int main(int argc, char* argv[]) {
     auto component_list =
