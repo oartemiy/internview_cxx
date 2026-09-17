fa60f72c0870fc8d9200f3b138706f8270482733
oartemiy <artemiy.obraztsov@yandex.ru>
2026-09-17 23:55:55 +0300

    Small changes

--- src/storages/interfaces/file_storage.hpp
+++ src/storages/interfaces/file_storage.hpp
@@ -1,15 +1,26 @@
 #pragma once
 
 #include <string>
-
-#include "utils/common_file_storage.hpp"
+#include "userver/server/http/form_data_arg.hpp"
 
 namespace internview::storages::interfaces {
 
 class IFileStorage {
 public:
+    struct File {
+        std::string_view data;
+        std::string filename;
+        std::optional<std::string> content_type;
+
+        File(const userver::server::http::FormDataArg& form_data_arg)
+            : data(form_data_arg.value),
+              filename(form_data_arg.filename ? *form_data_arg.filename : ""),
+              content_type(form_data_arg.content_type) {
+        }
+    };
+
     virtual ~IFileStorage() = default;
-    
+
     virtual std::string Save(const File& file) = 0;
 
     virtual std::string Load(const std::string& key) = 0;
